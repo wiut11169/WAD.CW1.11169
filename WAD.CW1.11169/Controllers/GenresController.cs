e9e8db03accfceb1732af959dcc555680cb28372
wiut11169 <wiut11169@gmail.com>
2024-03-19 16:34:26 +0500

    Adding controllers

---
+++ WAD.CW1.11169/Controllers/GenresController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD.CW1._11169.DAL.Dtos;
+using WAD.CW1._11169.DAL.Interfaces;
+using WAD.CW1._11169.DAL.Models;
+
+namespace WAD.CW1._11169.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly IGenreRepository _genreRepository;
+        private readonly IMapper _mapper;
+
+        public GenresController(IGenreRepository genreRepository, IMapper mapper)
+        {
+            _genreRepository = genreRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GenreDto>>> GetAllGenres()
+        {
+            var genres = await _genreRepository.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<GenreDto>>(genres));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GenreDto>> GetGenre(int id)
+        {
+            var genre = await _genreRepository.GetByIdAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<GenreDto>(genre));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GenreDto>> CreateGenre(GenreDto genreDto)
+        {
+            var genre = _mapper.Map<Genre>(genreDto);
+            await _genreRepository.CreateAsync(genre);
+            return CreatedAtAction(nameof(GetGenre), new { id = genre.Id }, _mapper.Map<GenreDto>(genre));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateGenre(int id, GenreDto genreDto)
+        {
+            if (id != genreDto.Id)
+            {
+                return BadRequest();
+            }
+
+            var genre = await _genreRepository.GetByIdAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(genreDto, genre);
+            await _genreRepository.UpdateAsync(genre);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGenre(int id)
+        {
+            var genre = await _genreRepository.GetByIdAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            await _genreRepository.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ WAD.CW1.11169/Controllers/MoviesController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD.CW1._11169.DAL.Dtos;
+using WAD.CW1._11169.DAL.Interfaces;
+using WAD.CW1._11169.DAL.Models;
+
+namespace WAD.CW1._11169.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+        private readonly IMapper _mapper;
+
+        public MoviesController(IMovieRepository movieRepository, IMapper mapper)
+        {
+            _movieRepository = movieRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MovieDto>>> GetAllMovies()
+        {
+            var movies = await _movieRepository.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<MovieDto>>(movies));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MovieDto>> GetMovie(int id)
+        {
+            var movie = await _movieRepository.GetByIdAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<MovieDto>(movie));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MovieDto>> CreateMovie(MovieDto movieDto)
+        {
+            var movie = _mapper.Map<Movie>(movieDto);
+            await _movieRepository.CreateAsync(movie);
+            return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, _mapper.Map<MovieDto>(movie));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMovie(int id, MovieDto movieDto)
+        {
+            if (id != movieDto.Id)
+            {
+                return BadRequest();
+            }
+
+            var movie = await _movieRepository.GetByIdAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(movieDto, movie);
+            await _movieRepository.UpdateAsync(movie);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            var movie = await _movieRepository.GetByIdAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            await _movieRepository.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
