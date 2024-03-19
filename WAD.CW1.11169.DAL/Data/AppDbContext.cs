8fa9e579109dec1edca15eabd9c5898694496377
wiut11169 <wiut11169@gmail.com>
2024-03-19 16:31:43 +0500

    Models with application db context have been developed

---
+++ WAD.CW1.11169.DAL/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WAD.CW1._11169.DAL.Models;
+
+namespace WAD.CW1._11169.DAL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>()
+                .HasOne(m => m.Genre)
+                .WithMany(g => g.Movies)
+                .HasForeignKey(m => m.GenreId);
+
+            // Seed data
+            modelBuilder.Entity<Genre>().HasData(
+                new Genre { Id = 1, Name = "Action" },
+                new Genre { Id = 2, Name = "Drama" }
+            );
+        }
+    }
+}
---
+++ WAD.CW1.11169.DAL/Models/Genre.cs
@@ -0,0 +1,9 @@
+﻿namespace WAD.CW1._11169.DAL.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Movie> Movies { get; set; }
+    }
+}
